2af74438ec6ff65069b3991cfc92184a7528c95c
Cyril K.B. <53737317+Cykyrios@users.noreply.github.com>
2024-02-14 21:30:58 +0100

    Add speed conversion functions

--- src/utils.gd
+++ src/utils.gd
@@ -2,6 +2,14 @@ class_name Utils
 extends RefCounted
 
 
+static func m_per_s_to_kph(value: float) -> float:
+	return value * 3.6
+
+
+static func m_per_s_to_mph(value: float) -> float:
+	return value * 2.236_936_292
+
+
 static func rad_per_second_to_rpm(value: float) -> float:
 	return value / (2 * PI) * 60
 
