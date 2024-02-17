61699e7f4f0c9bf69c20558cd29e551b5a1e28e8
Cyril K.B. <53737317+Cykyrios@users.noreply.github.com>
2024-02-17 14:43:12 +0100

    Add lap time string from float

--- src/utils.gd
+++ src/utils.gd
@@ -10,6 +10,14 @@ static func m_per_s_to_mph(value: float) -> float:
 	return value * 2.236_936_292
 
 
+static func get_lap_time_string(lap_time: float) -> String:
+	if lap_time < 0 or lap_time >= 6000:
+		return "00:00.00"
+	var minutes := floori(lap_time / 60)
+	var seconds := lap_time - minutes * 60
+	return "%s%05.2f" % ["" if lap_time < 60 else "%d:" % [minutes], seconds]
+
+
 static func rad_per_second_to_rpm(value: float) -> float:
 	return value / (2 * PI) * 60
 
