ed9ae979c50e04e0a4e15a5c953e4f4a76f45276
Cyril K.B. <53737317+Cykyrios@users.noreply.github.com>
2024-04-07 17:10:14 +0200

    Fix "lap overflow"
    I swear this time it's properly fixed.
    Elapsed time offset is not needed anymore.

--- src/telemetry/recorder.gd
+++ src/telemetry/recorder.gd
@@ -4,10 +4,6 @@ extends RefCounted
 
 signal lap_data_written
 
-## Session time vs InSim time offset (ms), usually 3 seconds in practice/quali, variable offset
-## in race, should be updated as soon as possible from SPX or LAP packets' elapsed time
-var elapsed_time_offset := 0
-
 var player_id := 0
 var current_lap: LapData = null
 var recording := false
@@ -21,27 +17,22 @@ var car := ""
 
 
 func delete_previous_lap_data() -> void:
-	if current_lap.total_time != 0 and not is_equal_approx(current_lap.total_time, 3600):
-		var outgauge_size := 0
-		var outsim_size := 0
-		for i in current_lap.outsim_data.size():
-			outsim_size += 1
-			if current_lap.outsim_data[-outsim_size].outsim_pack.time == \
-					int(1000 * current_lap.total_time) + elapsed_time_offset:
-				break
-		for i in current_lap.outgauge_data.size():
-			outgauge_size += 1
-			if current_lap.outgauge_data[-outgauge_size].time == \
-					int(1000 * current_lap.total_time) + elapsed_time_offset:
-				break
-		if not current_lap.outgauge_data.is_empty():
-			current_lap.outgauge_data.reverse()
-			var _discard := current_lap.outgauge_data.resize(outgauge_size)
-			current_lap.outgauge_data.reverse()
-		if not current_lap.outsim_data.is_empty():
-			current_lap.outsim_data.reverse()
-			var _discard := current_lap.outsim_data.resize(outsim_size)
-			current_lap.outsim_data.reverse()
+	var half_data_index := int(current_lap.outsim_data.size() / 2.0)
+	var half_data_distance := current_lap.outsim_data[half_data_index].outsim_pack.current_lap_distance
+	var idx := 0
+	while current_lap.outsim_data[-1 - idx].outsim_pack.current_lap_distance < half_data_distance:
+		idx += 1
+	var outsim_size := current_lap.outsim_data.size() - idx
+	var outsim_time := current_lap.outsim_data[-1 - idx].outsim_pack.time
+	var outgauge_size := current_lap.outgauge_data.size()
+	for i in outgauge_size:
+		if current_lap.outgauge_data[-1 - i].time == outsim_time:
+			outgauge_size -= i
+			break
+	if not current_lap.outgauge_data.is_empty():
+		current_lap.outgauge_data = current_lap.outgauge_data.slice(outgauge_size)
+	if not current_lap.outsim_data.is_empty():
+		current_lap.outsim_data = current_lap.outsim_data.slice(outsim_size)
 	current_lap.car_data.clear()
 	current_lap.date = ""
 	current_lap.lap_number = 0
@@ -109,7 +100,6 @@ func save_outsim_packet(packet: OutSimPacket) -> void:
 func save_sector(packet: InSimSPXPacket) -> void:
 	if not recording or packet.player_id != player_id:
 		return
-	update_elapsed_time_offset(packet.elapsed_time)
 	var sector_data := SectorData.new()
 	sector_data.sector_number = packet.split
 	sector_data.split_time = packet.split_time / 1000.0
@@ -124,7 +114,6 @@ func save_sector(packet: InSimSPXPacket) -> void:
 func save_lap(packet: InSimLAPPacket) -> void:
 	if not recording or packet.player_id != player_id:
 		return
-	update_elapsed_time_offset(packet.elapsed_time)
 	current_lap.lap_number = packet.laps_done
 	current_lap.lap_time = packet.lap_time / 1000.0
 	current_lap.total_time = packet.elapsed_time / 1000.0
@@ -149,17 +138,3 @@ func stop_recording() -> void:
 	end_current_lap()
 	recording = false
 	EventBus.telemetry_ended.emit()
-
-
-## Updates the session time vs InSim time offset for practice/quali/race sessions, where
-## offset is 3 seconds for practice and quali, and a variable amount for races.[br]
-## This function relies on being called as soon as a LAP or SPX packet is received,
-## with a variable delay assumed to be much less than the rounding performed
-## (offset is always an integer amount of seconds).
-func update_elapsed_time_offset(elapsed_time: int) -> void:
-	if not recording or current_lap.outgauge_data.is_empty():
-		return
-	if elapsed_time == 3_600_000:
-		return
-	var outgauge_time := current_lap.outgauge_data[-1].time
-	elapsed_time_offset = 1000 * roundi((outgauge_time - elapsed_time) / 1000.0)
